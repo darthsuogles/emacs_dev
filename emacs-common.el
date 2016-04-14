c3b2f4ee4da0e06141f61a16ed95b93b4be54dd1
Philip Yang <philip@quantifind.com>
2016-04-14 14:52:40 -0700

    Automatic dependency management
    Also moved scala functions to scala-custom.el

--- emacs-common.el
+++ emacs-common.el
@@ -5,18 +5,30 @@
 (setq elisp_path "~/CodeBase/emacs_dev")
 (add-to-list 'load-path elisp_path)
 
+;; Very basic key modifications for OSX
+;; Duplicated at the end of the file for fear that some mode might modify them
+(setq mac-option-key-is-meta t)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'meta)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs server
 (server-start)
 
 ;;--------------------------------------------------------------------
-;; Also add the package
+;; Package managing 
+;; (eval-when-compile
+;;   (require 'use-package))
+;; (require 'diminish)                ;; if you use :diminish
+;; (require 'bind-key)                ;; if you use any :bind variant
+
 (require 'package)
 (add-to-list 'load-path (concat elisp_path "/use-package"))
 (setq
  use-package-always-ensure t
  package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-		    ("org" . "http://orgmode.org/elpa/")
+                    ("org" . "http://orgmode.org/elpa/")
                     ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
@@ -28,18 +40,19 @@
 (dired-async-mode 1) ;; 
 
 (defvar package-depslist
-  '(helm s company magit projectile dash async use-package evil
-	 web-mode ess lua-mode z3-mode
-	 ensime scala-mode2 sbt-mode
-	 solarized-theme xterm-color)
+  '(helm s company magit projectile dash async 
+         use-package evil helm-flx
+         web-mode ess lua-mode z3-mode
+         ensime scala-mode2 sbt-mode
+         solarized-theme xterm-color)
   "A list of dependencies to be installed")
 
 (require 'cl) ;; use the common-lisp extension
 (defun package-deps-installed-p ()
   "Return true if the dependencies are all installed"
   (loop for pkg in package-depslist
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
 
 (unless (package-deps-installed-p)
   (package-refresh-contents)
@@ -47,7 +60,21 @@
     (when (not (package-installed-p pkg))
       (package-install pkg))))
 
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+         ("C-x C-m" . helm-M-x)
+         ("C-c C-m" . helm-M-x)
+         ("C-x b" . helm-mini)
+         ("C-x C-f" . helm-find-files)
+         ;;:map helm-mode-map
+         ("C-c h" . helm-execute-persistent-action))
+)
+
+(use-package company
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  ;;(add-hook 'after-init-hook 'company-statistics-mode t)
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;--------------------------------------------------------------------
@@ -55,191 +82,65 @@
 
 ;; comint install
 ;; Also remember to set TERM accordingly (xterm-256color)
-(add-to-list 'load-path (concat elisp_path "/xterm-color"))
-(require 'xterm-color)
-(progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
-       (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region))
-
-;; ;; comint uninstall
-;; (progn (remove-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-;;        (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
-;;        (setq font-lock-unfontify-region-function 'font-lock-default-unfontify-region))
-
+(use-package xterm-color
+  :load-path "xterm-color/"
+  :config
+  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+  (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
+  (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
+)
 
 ;; You can also use it with eshell (and thus get color output from system ls):
-(require 'eshell)
-(add-hook 'eshell-mode-hook
-          (lambda ()
-            (setq xterm-color-preserve-properties t)))
-(add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
-(setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
+(use-package eshell
+  :init
+  (add-hook 'eshell-mode-hook
+            (lambda () (setq xterm-color-preserve-properties t)))
+  :config
+  (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
+  (setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;--------------------------------------------------------------------
 ;; IPython
 ;;
 ;; Warning: do not use the master branch python.el
-;;          do not use el-get to install python
 ;; Please byte-compile the emacs-24 branch
-;;(load (concat elisp_path "/python.el"))
-(require 'python)
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args "--pylab"
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
- "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
- "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
- "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  :init
+  (setq
+   python-shell-interpreter "ipython"
+   python-shell-interpreter-args "--pylab"
+   python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+   python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+   python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+   python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+   python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+  )
 
 ;;--------------------------------------------------------------------
 ;; Scala
-;; <<<<<<< HEAD
-;; (require 'ensime)
-;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-;; (add-to-list 'auto-mode-alist '("\\.sclpt\\'" . scala-mode))
-;; =======
-(require 'scala-mode2)
-(defun scala-spark-shell ()
-  (interactive)
-  (scala-run-scala "~/spark/spark-shell.sh"))
-(defun scala-mode2-keymap ()
-  "Modify mode for scala sbt"
-  (local-set-key (kbd "C-c C-r") 'scala-eval-region)
-  (local-set-key (kbd "C-c C-c") 'scala-eval-definition)
-  (local-set-key (kbd "C-c C-z") 'scala-spark-shell)
-  ;; more here
-  )
-(add-hook 'scala-mode-hook 'scala-mode2-keymap)
-
-;; (require 'ensime)
-;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-;; >>>>>>> 78a6cb7cce819b9afc36f913e2b2ec45c0e3b12e
-
-(add-to-list 'load-path (concat elisp_path "/emacs-sbt-mode"))
-(use-package sbt-mode
-  :commands sbt-start sbt-command
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map))
-
-(defun sbt:paste-region (start end &optional no-exit)
-  "Send region (from START to END) using :paste REPL command.
-
-Using the EOF mode instead of C-D to avoid troubles
-
-If NO-EXIT is non-zero, this function will not end the paste
-mode."
-  (unless (comint-check-proc (sbt:buffer-name))
-    (error "sbt is not running in buffer %s" (sbt:buffer-name)))
-  (save-excursion
-    (goto-char end)
-    (skip-syntax-forward ">")
-    (forward-comment (- (point-max)))
-    (setq end (point)))
-  (save-excursion
-    (goto-char start)
-    (forward-comment (point-max))
-    (setq start (point)))
-  (unless (> end start) (error "mark a region of code first"))
-  (display-buffer (sbt:buffer-name))
-  (let ((submode (buffer-local-value 'sbt:submode
-                                     (get-buffer (sbt:buffer-name)))))    
-    (while (eq submode 'sbt)
-      (sbt-command "console") (error "must wait till we are in console mode"))
-    (message "submode: %s" submode)
-    (unless (eq submode 'paste-mode)
-      (comint-send-string (sbt:buffer-name) ":paste\n"))
-    (comint-send-region (sbt:buffer-name) start end)
-    (message "sbt code segment sent")
-    (comint-send-string (sbt:buffer-name) "\n\^D")
-    (message "sbt console paste termination signal sent")) 
-  )
-
-(defun sbt:paste-region-ammonite (start end)
-  "Send region (from START to END) using :paste REPL command."
-  (unless (comint-check-proc (sbt:buffer-name))
-    (error "sbt is not running in buffer %s" (sbt:buffer-name)))  
-  (save-excursion
-    (goto-char end)
-    (skip-syntax-forward ">")
-    (forward-comment (- (point-max)))
-    (setq end (point)))
-  (save-excursion
-    (goto-char start)
-    (forward-comment (point-max))
-    (setq start (point)))
-  (unless (> end start) (error "mark a region of code first"))
-  (display-buffer (sbt:buffer-name))
-  (let ((submode (buffer-local-value 'sbt:submode
-                                     (get-buffer (sbt:buffer-name)))))    
-    (while (eq submode 'sbt)
-      (sbt-command "console") (error "must wait till we are in console mode"))
-    (message "submode: %s" submode)
-    (unless (eq submode 'paste-mode)
-      (comint-send-string (sbt:buffer-name) ":paste\n"))
-    (comint-send-region (sbt:buffer-name) start end)
-    (message "sbt code segment sent")
-    (comint-send-string (sbt:buffer-name) "\n\^D")
-    (message "sbt console paste termination signal sent")) 
-  )
-
-;; Scala custom keys
-(defun scala-insert-arrow-assign () (interactive) (insert "<-"))
-(defun scala-mode-keymap ()
-  ""
-  (local-set-key (kbd "M-_") 'scala-insert-arrow-assign)
-  )
-(add-hook 'scala-mode-hook 'scala-mode-keymap)
-
-;; Scal SBT mode custom keys
-(defun scala-sbt-mode-keymap ()
-  "Modify mode for scala sbt"
-  (local-set-key (kbd "C-c C-b C-r") 'sbt-paste-region)
-  (local-set-key (kbd "C-c C-b C-c") 'sbt-start)
-  ;; more here
-  )
-;; add to hook
-;;(add-hook 'scala-mode-hook 'scala-sbt-mode-keymap)
-
+(load "scala-custom.el")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org-mode (site-lisp is the install location)
 (load "orgmode-custom.el")
 
-;;--------------------------------------------------------------------
-;; R (ESS mode) now in melpa
-;; (setq ess-lispdir (concat elisp_path "/ESS/lisp"))
-;; (add-to-list 'load-path ess-lispdir)
-;; (load (concat ess-lispdir "/ess-site.el"))
-
 ;; web-mode @ http://web-mode.org
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
-(defun web-mode-custom-hook ()
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-indent-style 2)
+(use-package web-mode
+  :mode ("\\.html?\\'" 
+         "\\.php\\'")
+  :init (setq
+         web-mode-markup-indent-offset 2
+         web-mode-css-indent-offset 2
+         web-mode-code-indent-offset 2
+         web-mode-indent-style 2)
+  :config (setq-default indent-tabs-mode t)
   )
-(add-hook 'web-mode-hook  'web-mode-custom-hook)
-(setq-default indent-tabs-mode t)
 
 ;; Coq
 (load (concat elisp_path "/prfgnrl/generic/proof-site.el"))
@@ -249,26 +150,28 @@ mode."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Flyspell for spell checking
 ;; Ref: http://www.emacswiki.org/emacs/FlySpell
-(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
-(autoload 'tex-mode-flyspell-verify "flyspell" "" t) 
-
-(add-hook 'LaTeX-mode-hook 'flyspell-mode)
-(dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
-(dolist (hook '(change-log-mode-hook log-edit-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode -1))))
-
-;;(require 'reftex)
-;; ;; spell checking for the comments
-;; (add-hook 'c++-mode-hook
-;;           (lambda ()
-;;             (flyspell-prog-mode)
-;; 	    ))
+(use-package flyspell
+  :commands (flyspell-delay-command tex-mode-flyspell-verify)
+  :config
+  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+  (setq flyspell-sort-corrections nil)
+  (setq flyspell-doublon-as-error-flag nil)
+  (dolist (hook '(text-mode-hook))
+    (add-hook hook (lambda () (flyspell-mode 1))))
+  (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
+    (add-hook hook (lambda () (flyspell-mode nil)))) 
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global Settings
 ;; - in case they are modified by other modes
+
+;; Substitude certain patterns (Greek letters or math symbols)
+;; with unicode
+(load "emacs-rc-pretty-lambda.el")
+
+;; New in Emacs 24, color theme, loaded at last
+(load-theme 'solarized-dark "NO-CONFIRM")
 
 ;; global variables
 (setq
@@ -289,7 +192,6 @@ mode."
 ;; modes
 (electric-indent-mode 0)
 
-
 ;;(tool-bar-mode nil) ;; turning off the tool-bar
 (transient-mark-mode t) 
 
@@ -301,16 +203,10 @@ mode."
 ;;(setq setq mac-command-key-is-meta t)
 ;;(setq mac-command-key 'meta)
 
-;; Substitude certain patterns (Greek letters or math symbols)
-;; with unicode
-(load "emacs-rc-pretty-lambda.el")
+(setq 
+ ring-bell-function 'ignore
+ visible-bell nil
+ global-visual-line-mode t
+ word-wrap t
+ ns-pop-up-frames nil)
 
-;; New in Emacs 24, color theme, loaded at last
-(load-theme 'solarized-dark "NO-CONFIRM")
-
-(setq ring-bell-function 'ignore)
-(setq visible-bell nil)
-(setq global-visual-line-mode t)
-(setq word-wrap t) ;; line wrapping
-
-(setq ns-pop-up-frames nil) ;; open new frames inside existing ones
---
+++ scala-custom.el
@@ -0,0 +1,98 @@
+;; additional .el load path
+(setq elisp_path "~/CodeBase/emacs_dev")
+
+;; <<<<<<< HEAD
+;; (require 'ensime)
+;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+;; (add-to-list 'auto-mode-alist '("\\.sclpt\\'" . scala-mode))
+;; =======
+(defun scala-insert-arrow-assign () (interactive) (insert "<-"))
+(defun scala-spark-shell () (interactive) (scala-run-scala "./spark-shell.sh"))
+
+(use-package scala-mode2
+  :mode ("\\.sclpt\\'" . scala-mode)
+  :bind (("C-c C-r" . scala-eval-region)
+         ("C-c C-c" . scala-eval-definition)
+         ("C-c C-z" . scala-spark-shell)
+	 ("M-_"     . scala-insert-arrow-assign))
+  )
+
+;; (require 'ensime)
+;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+;; >>>>>>> 78a6cb7cce819b9afc36f913e2b2ec45c0e3b12e
+
+(add-to-list 'load-path (concat elisp_path "/emacs-sbt-mode"))
+;; Scal SBT mode custom keys
+(use-package sbt-mode
+  :commands sbt-start sbt-command  
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+  :bind (("C-c C-b C-r" . sbt-paste-region)
+         ("C-c C-b C-c" . sbt-start))
+  )
+
+(defun sbt:paste-region (start end &optional no-exit)
+  "Send region (from START to END) using :paste REPL command.
+
+Using the EOF mode instead of C-D to avoid troubles
+
+If NO-EXIT is non-zero, this function will not end the paste
+mode."
+  (unless (comint-check-proc (sbt:buffer-name))
+    (error "sbt is not running in buffer %s" (sbt:buffer-name)))
+  (save-excursion
+    (goto-char end)
+    (skip-syntax-forward ">")
+    (forward-comment (- (point-max)))
+    (setq end (point)))
+  (save-excursion
+    (goto-char start)
+    (forward-comment (point-max))
+    (setq start (point)))
+  (unless (> end start) (error "mark a region of code first"))
+  (display-buffer (sbt:buffer-name))
+  (let ((submode (buffer-local-value 'sbt:submode
+                                     (get-buffer (sbt:buffer-name)))))    
+    (while (eq submode 'sbt)
+      (sbt-command "console") (error "must wait till we are in console mode"))
+    (message "submode: %s" submode)
+    (unless (eq submode 'paste-mode)
+      (comint-send-string (sbt:buffer-name) ":paste\n"))
+    (comint-send-region (sbt:buffer-name) start end)
+    (message "sbt code segment sent")
+    (comint-send-string (sbt:buffer-name) "\n\^D")
+    (message "sbt console paste termination signal sent")) 
+  )
+
+(defun sbt:paste-region-ammonite (start end)
+  "Send region (from START to END) using :paste REPL command."
+  (unless (comint-check-proc (sbt:buffer-name))
+    (error "sbt is not running in buffer %s" (sbt:buffer-name)))  
+  (save-excursion
+    (goto-char end)
+    (skip-syntax-forward ">")
+    (forward-comment (- (point-max)))
+    (setq end (point)))
+  (save-excursion
+    (goto-char start)
+    (forward-comment (point-max))
+    (setq start (point)))
+  (unless (> end start) (error "mark a region of code first"))
+  (display-buffer (sbt:buffer-name))
+  (let ((submode (buffer-local-value 'sbt:submode
+                                     (get-buffer (sbt:buffer-name)))))    
+    (while (eq submode 'sbt)
+      (sbt-command "console") (error "must wait till we are in console mode"))
+    (message "submode: %s" submode)
+    (unless (eq submode 'paste-mode)
+      (comint-send-string (sbt:buffer-name) ":paste\n"))
+    (comint-send-region (sbt:buffer-name) start end)
+    (message "sbt code segment sent")
+    (comint-send-string (sbt:buffer-name) "\n\^D")
+    (message "sbt console paste termination signal sent")) 
+  )
